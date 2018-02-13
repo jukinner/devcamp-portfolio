9a1e45e65e2967c8ef5a7f9f3935b166ed843ef4
Jukinner <jukinner@gmail.com>
2018-02-13 15:23:54 -0700

    Implemented action cable for comments

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -1,3 +1,4 @@
+
 class CommentBroadcastJob < ApplicationJob
   queue_as :default
 
@@ -10,5 +11,4 @@ class CommentBroadcastJob < ApplicationJob
   def render_comment(comment)
     CommentsController.render partial: 'comments/comment', locals: { comment: comment }
   end
-
 end
